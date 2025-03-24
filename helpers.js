31e0e497b8573da7b28beece0948220233040346
Bogdan <herecomesthefog@gmail.com>
2025-03-24 14:51:44 +0500

    add cheerio, change filepaths

--- helpers.js
+++ helpers.js
@@ -1,23 +1,28 @@
 import * as fs from "node:fs";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
+import * as cheerio from "cheerio";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export function changeStylesLink(filePath, fileName) {
-    let htmlFile = fs.readFileSync(filePath, {encoding: 'utf-8'});
-    let fileContentArr = htmlFile.split('\n');
+export function changePaths(filePath, fileName, stylePath, imagesSrc) {
+    const htmlFile = fs.readFileSync(filePath);
+    const $ = cheerio.loadBuffer(htmlFile);
 
-    fileContentArr.forEach(item => {
-        if (item.match('./css/styles.css')) {
-            let itemIndex = fileContentArr.indexOf(item);
-            fileContentArr[itemIndex] = '<link rel="stylesheet" href="styles.css">';
+    $(`link[href="./${stylePath}"]`).attr('href', 'styles.css');
+
+    $('img').each((i, el) => {
+        let currentSrc = $(el).attr('src');
+
+        if (currentSrc.match(imagesSrc)) {
+            let imageName = currentSrc.split('/').pop();
+            $(el).attr('src', `images/${imageName}`);
         }
     })
 
     if (!fs.existsSync(`${__dirname}/dist`)) {
         fs.mkdirSync(`${__dirname}/dist`)
     }
-    fs.writeFileSync(`./dist/${fileName}`, fileContentArr.join('\n'));
+    fs.writeFileSync(`./dist/${fileName}`, $.html());
 }
